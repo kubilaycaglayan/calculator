bd75d60f410e2a32c4649cf3b1e0c123b576b433
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-24 15:49:28 +0300

    Turn App function into class

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,30 @@ import ButtonPanel from './ButtonPanel';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-export default function App() {
-  return (
-    <div id="app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operate: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operate } = this.state;
+    console.log('clicked ', total, next, operate, buttonName);
+  }
+
+  render() {
+    const { next } = this.state;
+    return (
+      <div id="app">
+        <Display next={next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
+
+export default App;
--- src/components/Display.js
+++ src/components/Display.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Display(props) {
-  const { result } = props;
+  const { next } = props;
   return (
     <div className="display">
-      { result }
+      { next }
     </div>
   );
 }
 
 Display.defaultProps = {
-  result: '0',
+  next: '0',
 };
 
 Display.propTypes = {
-  result: PropTypes.string,
+  next: PropTypes.string,
 };
