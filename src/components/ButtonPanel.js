ed2cf40e4d58f674f6077a9cd3ed1c203683f5a0
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-24 15:52:57 +0300

    Add clickhandler into Button Panel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,31 +2,35 @@ import React from 'react';
 import Button from './Button';
 
 export default function ButtonPanel() {
+  function handleClick(buttonName) {
+    return this.props.clickHandler(buttonName);
+  }
+
   return (
     <div className="button-panel">
       <div>
-        <Button color="true" name="AC" />
-        <Button color="true" name="+/-" />
-        <Button color="true" name="%" />
-        <Button name="÷" />
+        <Button clickHandler={handleClick} color="true" name="AC" />
+        <Button clickHandler={handleClick} color="true" name="+/-" />
+        <Button clickHandler={handleClick} color="true" name="%" />
+        <Button clickHandler={handleClick} name="÷" />
       </div>
       <div>
-        <Button color="true" name="7" />
-        <Button color="true" name="8" />
-        <Button color="true" name="9" />
-        <Button name="X" />
+        <Button clickHandler={handleClick} color="true" name="7" />
+        <Button clickHandler={handleClick} color="true" name="8" />
+        <Button clickHandler={handleClick} color="true" name="9" />
+        <Button clickHandler={handleClick} name="X" />
       </div>
       <div>
-        <Button color="true" name="4" />
-        <Button color="true" name="5" />
-        <Button color="true" name="6" />
-        <Button name="-" />
+        <Button clickHandler={handleClick} color="true" name="4" />
+        <Button clickHandler={handleClick} color="true" name="5" />
+        <Button clickHandler={handleClick} color="true" name="6" />
+        <Button clickHandler={handleClick} name="-" />
       </div>
       <div>
-        <Button color="true" name="1" />
-        <Button color="true" name="2" />
-        <Button color="true" name="3" />
-        <Button name="+" />
+        <Button clickHandler={handleClick} color="true" name="1" />
+        <Button clickHandler={handleClick} color="true" name="2" />
+        <Button clickHandler={handleClick} color="true" name="3" />
+        <Button clickHandler={handleClick} name="+" />
       </div>
       <div>
         <Button color="true" wide="true" name="0" />
