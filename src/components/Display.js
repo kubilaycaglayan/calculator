ca5e818165a3ece423ec47f5270add7770d779fb
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-25 09:58:25 +0300

    Add operate and total view into Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,18 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Display(props) {
-  const { next } = props;
+  const { total, next, operate } = props;
   return (
-    <div className="display">
-      { next }
+    <div>
+      <div className="operation">
+        <div className="total">
+          { total }
+          72
+        </div>
+        <div className="operate">
+          { operate }
+          +
+        </div>
+      </div>
+      <div className="display">
+        { next }
+      </div>
     </div>
   );
 }
 
 Display.defaultProps = {
+  total: '',
   next: '0',
+  operate: '',
 };
 
 Display.propTypes = {
   next: PropTypes.string,
+  total: PropTypes.string,
+  operate: PropTypes.string,
 };
