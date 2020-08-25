116c23bc39244f5fa92818c747d032e6015b3ff2
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-25 16:59:30 +0300

    Refactor: prevenDefault isolated for only enter button

--- src/events/keyboard.js
+++ src/events/keyboard.js
@@ -1,10 +1,10 @@
 const handleButtons = (event, callback) => {
-  event.preventDefault();
   let buttonName = event.key;
   if (!['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
     'Enter', '/', '*', ',', 'Backspace', '+', '-'].includes(buttonName)) return;
   switch (buttonName) {
     case 'Enter':
+      event.preventDefault();
       buttonName = '=';
       break;
     case '/':
