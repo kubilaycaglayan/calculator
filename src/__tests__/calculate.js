6cd1da5b095eca1a85afa836b319f8afe017772b
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-26 15:33:33 +0300

    Update test: calculate

--- src/__tests__/calculate.js
+++ src/__tests__/calculate.js
@@ -3,13 +3,19 @@ import operate from '../logic/calculate';
 describe('calculate', () => {
   describe('accepts data object and buttonName as parameters', () => {
     const data = {
-      total: 10,
-      next: 20,
+      total: '10',
+      next: '20',
       operate: '',
+    };
+    const newData = {
+      total: '30',
+      next: '30',
+      operate: '+',
+      clear: true,
     };
     const buttonName = '+';
     it('returns the result of the calculation', () => {
-      expect(operate(data, buttonName)).toBe('30');
+      expect(operate(data, buttonName)).toEqual(newData);
     });
   });
 });
